32b4606527b513c75f6d6b1a76ce644bdf38ec6a
Timothy Reen <reentim@gmail.com>
2025-03-04 15:37:19 +1100

    Roughly build out deck list cards

--- src/components/deckListCard.tsx
+++ src/components/deckListCard.tsx
@@ -3,6 +3,7 @@ import { Link } from 'wouter-preact';
 import DeckData from '../deckData';
 
 import './deckListCard.css';
+import { PencilSquareIcon } from '@heroicons/react/24/outline';
 
 export const DeckListCard = ({ deck }: { deck: DeckData }) => {
   return (
@@ -11,6 +12,9 @@ export const DeckListCard = ({ deck }: { deck: DeckData }) => {
         <Link to={`/decks/${deck.id}`}>{deck.name}</Link>
       </h3>
       {deck.size} cards
+      <Link className="editDeckListCard" to={`/decks/${deck.id}/edit`}>
+        <PencilSquareIcon className="icon" />
+      </Link>
     </div>
   );
 };
